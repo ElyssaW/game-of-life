8e5fccdc9cceb38c42a365701f4f5cf4d8ea6fcc
ElyssaW <ejwinch1@gmail.com>
2021-04-15 16:02:33 -0500

    Basic game implementation

--- readme.md
+++ readme.md
@@ -7,3 +7,27 @@ A WIP browser-based implementation of Conway's Game of Life, made with Canvas.
 -    Any live cell with fewer than two live neighbours dies, as if by underpopulation.
 -    Any live cell with two or three live neighbours lives on to the next generation.
 -    Any live cell with more than three live neighbours dies, as if by overpopulation.
+-    Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+## Methodology
+
+#### The Board
+
+The first step is to determine how to represent the game board - I choose a 2-dimensional array, since the value of each square in the array is just a simple boolean of "alive" or "dead." The X value is the outer array, the Y value is the inner - or, in other words, columns and then rows.
+
+#### The Check
+
+The second step is how to check the neighboring cells of each square on the gameboard, so that we can apply the rules of the game to it as necessary.
+
+I choose to use nested for-loops, the outer with a range of x-1 to x+1, and and the inner with a range of y-1 to y+1. The inner loop would access the board at square [i, j] and each square, instead of holding a True or False, can hold a 1 or 0 to simulate Alive and Dead. The sum of all checked squares will tell us how many Alive cells surround the center cell, and thus, which rule it falls under.
+
+But what about bounds-checking? This method only works for middle cells - cells on the first column, last column, first row, or last row will throw an out-of-bounds error when we access the array. This could've been solved with lots of if statements, but that got messy fast. Instead, I added extra border rows/columns, and have the check logic always skip the first and last row and column.
+
+#### The Rules
+
+The next step is to put the rules into programmable logic. In order:
+
+- If the sum is < 2, the cell dies
+- If the sum is 2 or 3, and the square holds a 1, the cell is alive
+- If the sum is > 3, the cell dies
+- If the sum is 3, the cell becomes alive
--- script.js
+++ script.js
@@ -1,29 +1,94 @@
 let c = document.getElementById("myCanvas")
 let ctx = c.getContext("2d")
 
-x = c.width / 20
-y = c.height / 10
+size = 80
+columns = c.width / size
+rows = c.height / size
 
-let loop
+function createBoard (numberOfColumns, numberOfRows, initialValue) {
+    let array = []
+
+    for (let i = 0; i < numberOfColumns; i++) {
+        let row = []
+
+        for (let j = 0; j < numberOfRows; j++) {
+            row.push(initialValue)
+        }
+
+        array.push(row)
+    }
+
+    return array
+}
+
+let board = createBoard(columns+2, rows+2, 0)
 
 function step () {
-    console.log('game')
+    ctx.clearRect(0, 0, 800, 400)
 
-    for (let i = 0; i < y; i++) {
-        for (let j = 0; j < x; j++) {
-            if ((i % 2 == 0 && j % 2 == 1) || (i % 2 == 1 && j % 2 == 0)) {
-                ctx.fillStyle = 'white'
-                ctx.fillRect(40 * i, 40 * j, 40, 40);
-            } else {
-                ctx.fillStyle = 'lightgrey'
-                ctx.fillRect(40 * i, 40 * j, 40, 40);
+    console.log('New step')
+
+    let newBoard = board.map(row => {
+        return [...row]
+    })
+
+    for (let x = 1; x < board.length - 1; x++) {
+        console.log(board[x])
+        for (let y = 1; y < board[x].length-1; y++) {
+
+            let sum = 
+                board[x-1][y] + board[x+1][y] +
+                board[x][y-1] + board[x][y+1] +
+                board[x-1][y-1] + board[x+1][y+1] +
+                board[x-1][y+1] + board[x+1][y-1]
+            
+            if (((sum === 2 && board[x][y] === 1) || sum === 3)) {
+                newBoard[x][y] = 1
+
+                ctx.fillStyle = 'black'
+                ctx.fillRect(size * (x), size * (y), size, size)
+                ctx.fillStyle = 'red'
+                ctx.font = "30px Arial";
+                ctx.fillText(`${x}, ${y}`, size * (x), size * (y)+40)
+
+            } else if (board[x][y] === 1) {
+                newBoard[x][y] = 0
             }
         }
     }
+
+    board = newBoard.map(row => {
+        return [...row]
+    })
 }
 
-function startGame () {
-    loop = setInterval(step, 500)
+myCanvas.onclick = function (e) {
+    let clickX = e.offsetX
+    let clickY = e.offsetY
+
+    board.forEach((row, x) => {
+        if (x == 0 || x == board.length-1) return;
+
+        row.forEach((square, y) => {
+            if (y == 0 || y == board.length[x]-1) return;
+
+            if (
+            clickX > (x) * size && clickX < (x) * size + size && 
+            clickY > (y) * size && clickY < (y) * size + size) {
+                board[x][y] = 1
+
+                ctx.fillStyle = 'black'
+                ctx.fillRect(size * (x), size * (y), size, size)
+                ctx.fillStyle = 'red'
+                ctx.font = "30px Arial";
+                ctx.fillText(`${x}, ${y}`, size * (x), size * (y)+40)
+            }
+        })
+    })
 }
 
-startGame()
+// function startGame () {
+//     loop = setInterval(step, 500)
+// }
+
+// startGame()
